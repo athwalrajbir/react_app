774c5c7a9986e141b51cb1daf46c95815179d2d4
Rajbir <rajbirathwal1@gmail.com>
2023-05-25 13:03:06 -0400

    Nested

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-// function App() {
-//   return <h1>Hello world</h1>;
-// }
-
 const App = () => {
-  return React.createElement("h1", {}, "Hello World");
+  return (
+    <>
+      <MyName />
+      <h1>Hello world</h1>;<p>Awesome</p>
+      <img src="" alt="" />
+    </>
+  );
 };
+
+const MyName = () => {
+  return <h1>Hello Rajbir</h1>;
+};
+
+// const App = () => {
+//   return React.createElement("h1", {}, "Hello World");
+// };
 
 export default App;
